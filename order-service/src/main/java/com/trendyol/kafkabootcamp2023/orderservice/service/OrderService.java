4775b6755e6bdfca927ec74cee375eae769e4484
Ahmet Furkan Kavraz <ahmetfurkankavraz@hotmail.com>
2023-09-24 16:28:49 +0300

    missing part

--- order-service/src/main/java/com/trendyol/kafkabootcamp2023/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/trendyol/kafkabootcamp2023/orderservice/service/OrderService.java
@@ -51,6 +51,6 @@ public class OrderService {
             orderDeliveredProducer.produce(new OrderDeliveredMessage(order));
         }
 
-        return null;
+        return new OrderResponse(order);
     }
 }
